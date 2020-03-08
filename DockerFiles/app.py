2a94e2e39fe1293629b2904dfe354fa2edcfcb9b
kenling <kenling@vt.edu>
2020-03-08 18:02:48 -0400

    test version of flask app

--- DockerFiles/app.py
+++ DockerFiles/app.py
@@ -1,18 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/users, methods=['GET']) 
+@app.route('/users', methods=['GET']) 
 def users_get():
     return 'Here are all of the users'
 
-@app.route('/users, methods=['POST'])
+@app.route('/users', methods=['POST'])
 def users_post():    
     return 'Thanks for creating a new user'
 
-@app.route('/greeting, methods=['GET'])
+@app.route('/greeting', methods=['GET'])
 def get_greeting():
-    return jsonify('Hello world!')
+    return jsonify("greeting")
+
